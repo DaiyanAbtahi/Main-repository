ea6bddca3f9c78a6894f8b255d541a9318f4d4fb
DaiyanAbtahi <nilu.nibirazzo@gmail.com>
2025-03-14 14:47:54 +0600

    .

--- calculator.py
+++ calculator.py
@@ -2,45 +2,59 @@ import os
 os.system('cls')
 
 
-def get_num(n : str | float ) -> str | float :
+def get_num(n: str) -> float:
     while True:
-            try:
-                num = float(input(f"Enter the {n} number :"))
-                break
-            except:
-                print("INVALID NUMBER.....PLEASE TRY AGAIN")
-                continue
-    return num 
-
-num1 = get_num("first")
-num2 = get_num("second")
+        try:
+            num = float(input(f"Enter the {n} number: "))
+            return num
+        except ValueError:
+            print("\nINVALID NUMBER.....PLEASE TRY AGAIN\n")
 
 
-
-operator = ("+" , "-" ,"/" , "*")
-
-while True:
-        op = input("Enter an operator ( + , - , / , *) :")
-
-        if op not in operator:
-                print("INVALID OPERATOR.....PLEASE TRY AGAIN")
-                print(op)
-        else:
-               break
+def get_operator() -> str:
+    operators = {"+", "-", "*", "/", "%", "**"}
+    while True:
+        op = input("Enter an operator (+, -, *, /, %): ")
+        if op in operators:
+            return op
+        print("\nINVALID OPERATOR.....PLEASE TRY AGAIN\n")
 
 
-if op == '+':
-        result = num1 + num2
-elif op == '-':
-        result = num1 - num2
-elif op == '*':
-        result = num1 * num2
-elif op == '/':
-        if num2 == 0 and op == "/" :
-                print("\nMATH ERROR\nDivision by zero is not possible because it leads to an undefined or infinite result.")
-                quit()
-        else:
-                result = num1 / num2
-                
+def calculate(num1: float, num2: float, op: str) -> float:
+    if op == '+':
+        return num1 + num2
+    elif op == '-':
+        return num1 - num2
+    elif op == '*':
+        return num1 * num2
+    elif op == '/':
+        if num2 == 0:
+            print("\nMATH ERROR: Division by zero is not possible.")
+            return float('nan')
+        return num1 / num2
+    elif op == '%':
+        return num1 % num2
 
-print(result)
+
+
+def main():
+    while True:
+        os.system('cls')
+        
+        num1 = get_num("first")
+        num2 = get_num("second")
+        op = get_operator()
+        
+        result = calculate(num1, num2, op)
+        
+        print(f"\nResult: {num1} {op} {num2} = {result}\n")
+        
+        retry = input("Would you like to perform another calculation? (yes/no): ").strip().lower()
+        if retry not in ('yes', 'y'):
+            break
+
+
+if __name__ == "__main__":
+    main()
+
+
